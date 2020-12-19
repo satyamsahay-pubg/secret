94e19b52db976c8db8b7f18c08dfa327ba66f9dc
satyam sahay <sahaysatyam1001@gmail.com>
2020-12-19 20:50:05 +0530

    add .env file and commit with level 1 and level 2 encryption

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-//jshint esversion:6
+//jshint esversion:
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -18,8 +19,8 @@ const userSchema = new mongoose.Schema({
   password:String
 });
 
-const secret = "thisisourlittlesecret";
-userSchema.plugin(encrypt,{secret: secret,encryptedFields: ['password']});
+
+userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields: ['password']});
 
 const User = new mongoose.model("User",userSchema);
 
